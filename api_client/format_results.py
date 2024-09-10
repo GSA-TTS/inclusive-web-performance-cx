ef7641cb92da199bc0a53b1fa5ce9e0643b00ac9
Ethan Gardner <edgincvg@gmail.com>
2024-09-09 22:06:40 -0400

    build dictionary instead of list

--- api_client/format_results.py
+++ api_client/format_results.py
@@ -7,20 +7,25 @@ def format_results(results):
     """Format results for csv file"""
     metrics = results["record"]["metrics"]
 
-    ttfb = metrics.get("experimental_time_to_first_byte", {})
-    fcp = metrics.get("first_contentful_paint", {})
-    lcp = metrics.get("largest_contentful_paint", {})
-    cls = metrics.get("cumulative_layout_shift", {})
-    inp = metrics.get("interaction_to_next_paint", {})
+    fields = [
+        "experimental_time_to_first_byte",
+        "first_contentful_paint",
+        "largest_contentful_paint",
+        "cumulative_layout_shift",
+        "interaction_to_next_paint",
+    ]
 
     data = {
         "url": results["record"]["key"]["url"],
-        "time_to_first_byte_p75": ttfb.get("percentiles", {}).get("p75", None),
-        "first_contentful_paint_p75": fcp.get("percentiles", {}).get("p75", None),
-        "largest_contentful_paint_p75": lcp.get("percentiles", {}).get("p75", None),
-        "cumulative_layout_shift_p75": cls.get("percentiles", {}).get("p75", None),
-        "interaction_to_next_paint_p75": inp.get("percentiles", {}).get("p75", None),
     }
+
+    for field in fields:
+        data[field + "_p75"] = (
+            metrics.get(field, {}).get("percentiles", {}).get("p75", None)
+        )
+        data.update(
+            categorize_histogram(field, metrics.get(field, {}).get("histogram", []))
+        )
 
     return data
 
@@ -36,7 +41,7 @@ def categorize_histogram(label, histogram):
             }
         )
     else:
-        histogram.sort(key=lambda x: x['start'])
+        histogram.sort(key=lambda x: x["start"])
 
         data.update(
             {
@@ -46,11 +51,8 @@ def categorize_histogram(label, histogram):
             }
         )
 
-    return dict
+    return data
+
 
 if __name__ == "__main__":
-    results = {'record': {'key': {'formFactor': 'PHONE', 'url': 'https://www.wikipedia.org/'}, 'metrics': {'first_contentful_paint': {'histogram': [{'start': 0, 'end': 1800, 'density': 0.8874}, {'start': 1800, 'end': 3000, 'density': 0.0729}, {'start': 3000, 'density': 0.0397}], 'percentiles': {'p75': 1126}}, 'first_input_delay': {'histogram': [{'start': 0, 'end': 100, 'density': 0.9797}, {'start': 100, 'end': 300, 'density': 0.0153}, {'start': 300, 'density': 0.005}], 'percentiles': {'p75': 12}}, 'interaction_to_next_paint': {'histogram': [{'start': 0, 'end': 200, 'density': 0.9255}, {'start': 200, 'end': 500, 'density': 0.058}, {'start': 500, 'density': 0.0164}], 'percentiles': {'p75': 104}}, 'largest_contentful_paint': {'histogram': [{'start': 0, 'end': 2500, 'density': 0.927}, {'start': 2500, 'end': 4000, 'density': 0.046}, {'start': 4000, 'density': 0.027}], 'percentiles': {'p75': 1276}}, 'navigation_types': {'fractions': {'navigate_cache': 0.3864, 'reload': 0.0376, 'restore': 0, 'back_forward': 0.0522, 'back_forward_cache': 0.189, 'prerender': 0.0887, 'navigate': 0.2461}}, 'round_trip_time': {'percentiles': {'p75': 144}}, 'cumulative_layout_shift': {'histogram': [{'start': '0.00', 'end': '0.10', 'density': 0.9931}, {'start': '0.10', 'end': '0.25', 'density': 0.0061}, {'start': '0.25', 'density': 0.0008}], 'percentiles': {'p75': '0.00'}}, 'experimental_time_to_first_byte': {'histogram': [{'start': 0, 'end': 800, 'density': 0.8677}, {'start': 800, 'end': 1800, 'density': 0.0893}, {'start': 1800, 'density': 0.043}], 'percentiles': {'p75': 507}}}, 'collectionPeriod': {'firstDate': {'year': 2024, 'month': 8, 'day': 11}, 'lastDate': {'year': 2024, 'month': 9, 'day': 7}}}, 'urlNormalizationDetails': {'originalUrl': 'https://www.wikipedia.org', 'normalizedUrl': 'https://www.wikipedia.org/'}}
-
-    print(format_results(results))
-
     pass
--- api_client/test/test_format_results.py
+++ api_client/test/test_format_results.py
@@ -5,17 +5,102 @@ from api_client.format_results import format_results
 def test_format_results_normal():
     results = {
         "record": {
-            "key": {"url": "https://example.com"},
+            "key": {"formFactor": "PHONE", "url": "https://www.wikipedia.org/"},
             "metrics": {
-                "experimental_time_to_first_byte": {"percentiles": {"p75": 100}},
-                "first_contentful_paint": {"percentiles": {"p75": 200}},
-                "largest_contentful_paint": {"percentiles": {"p75": 300}},
-                "cumulative_layout_shift": {"percentiles": {"p75": 400}},
-                "interaction_to_next_paint": {"percentiles": {"p75": 500}},
+                "first_contentful_paint": {
+                    "histogram": [
+                        {"start": 0, "end": 1800, "density": 0.8874},
+                        {"start": 1800, "end": 3000, "density": 0.0729},
+                        {"start": 3000, "density": 0.0397},
+                    ],
+                    "percentiles": {"p75": 1126},
+                },
+                "first_input_delay": {
+                    "histogram": [
+                        {"start": 0, "end": 100, "density": 0.9797},
+                        {"start": 100, "end": 300, "density": 0.0153},
+                        {"start": 300, "density": 0.005},
+                    ],
+                    "percentiles": {"p75": 12},
+                },
+                "interaction_to_next_paint": {
+                    "histogram": [
+                        {"start": 0, "end": 200, "density": 0.9255},
+                        {"start": 200, "end": 500, "density": 0.058},
+                        {"start": 500, "density": 0.0164},
+                    ],
+                    "percentiles": {"p75": 104},
+                },
+                "largest_contentful_paint": {
+                    "histogram": [
+                        {"start": 0, "end": 2500, "density": 0.927},
+                        {"start": 2500, "end": 4000, "density": 0.046},
+                        {"start": 4000, "density": 0.027},
+                    ],
+                    "percentiles": {"p75": 1276},
+                },
+                "navigation_types": {
+                    "fractions": {
+                        "navigate_cache": 0.3864,
+                        "reload": 0.0376,
+                        "restore": 0,
+                        "back_forward": 0.0522,
+                        "back_forward_cache": 0.189,
+                        "prerender": 0.0887,
+                        "navigate": 0.2461,
+                    }
+                },
+                "round_trip_time": {"percentiles": {"p75": 144}},
+                "cumulative_layout_shift": {
+                    "histogram": [
+                        {"start": "0.00", "end": "0.10", "density": 0.9931},
+                        {"start": "0.10", "end": "0.25", "density": 0.0061},
+                        {"start": "0.25", "density": 0.0008},
+                    ],
+                    "percentiles": {"p75": "0.00"},
+                },
+                "experimental_time_to_first_byte": {
+                    "histogram": [
+                        {"start": 0, "end": 800, "density": 0.8677},
+                        {"start": 800, "end": 1800, "density": 0.0893},
+                        {"start": 1800, "density": 0.043},
+                    ],
+                    "percentiles": {"p75": 507},
+                },
             },
-        }
+            "collectionPeriod": {
+                "firstDate": {"year": 2024, "month": 8, "day": 11},
+                "lastDate": {"year": 2024, "month": 9, "day": 7},
+            },
+        },
+        "urlNormalizationDetails": {
+            "originalUrl": "https://www.wikipedia.org",
+            "normalizedUrl": "https://www.wikipedia.org/",
+        },
     }
-    expected_data = ["https://example.com", 100, 200, 300, 400, 500]
+    expected_data = {
+        "url": "https://www.wikipedia.org/",
+        "experimental_time_to_first_byte_p75": 507,
+        "experimental_time_to_first_byte_good_density": 0.8677,
+        "experimental_time_to_first_byte_needs_improvement_density": 0.0893,
+        "experimental_time_to_first_byte_poor_density": 0.043,
+        "first_contentful_paint_p75": 1126,
+        "first_contentful_paint_good_density": 0.8874,
+        "first_contentful_paint_needs_improvement_density": 0.0729,
+        "first_contentful_paint_poor_density": 0.0397,
+        "largest_contentful_paint_p75": 1276,
+        "largest_contentful_paint_good_density": 0.927,
+        "largest_contentful_paint_needs_improvement_density": 0.046,
+        "largest_contentful_paint_poor_density": 0.027,
+        "cumulative_layout_shift_p75": "0.00",
+        "cumulative_layout_shift_good_density": 0.9931,
+        "cumulative_layout_shift_needs_improvement_density": 0.0061,
+        "cumulative_layout_shift_poor_density": 0.0008,
+        "interaction_to_next_paint_p75": 104,
+        "interaction_to_next_paint_good_density": 0.9255,
+        "interaction_to_next_paint_needs_improvement_density": 0.058,
+        "interaction_to_next_paint_poor_density": 0.0164,
+    }
     assert format_results(results) == expected_data
 
 
@@ -28,18 +113,29 @@ def test_format_results_missing_metrics():
             },
         }
     }
-    expected_data = ["https://example.com", 100, None, None, None, None]
-    assert format_results(results) == expected_data
-
-
-def test_format_results_no_data():
-    results = {
-        "record": {
-            "key": {"url": "https://example.com"},
-            "metrics": {},
-        }
+    expected_data = {
+        "cumulative_layout_shift_good_density": None,
+        "cumulative_layout_shift_needs_improvement_density": None,
+        "cumulative_layout_shift_p75": None,
+        "cumulative_layout_shift_poor_density": None,
+        "experimental_time_to_first_byte_good_density": None,
+        "experimental_time_to_first_byte_needs_improvement_density": None,
+        "experimental_time_to_first_byte_p75": 100,
+        "experimental_time_to_first_byte_poor_density": None,
+        "first_contentful_paint_good_density": None,
+        "first_contentful_paint_needs_improvement_density": None,
+        "first_contentful_paint_p75": None,
+        "first_contentful_paint_poor_density": None,
+        "interaction_to_next_paint_good_density": None,
+        "interaction_to_next_paint_needs_improvement_density": None,
+        "interaction_to_next_paint_p75": None,
+        "interaction_to_next_paint_poor_density": None,
+        "largest_contentful_paint_good_density": None,
+        "largest_contentful_paint_needs_improvement_density": None,
+        "largest_contentful_paint_p75": None,
+        "largest_contentful_paint_poor_density": None,
+        "url": "https://example.com",
     }
-    expected_data = ["https://example.com", None, None, None, None, None]
     assert format_results(results) == expected_data
 
 
